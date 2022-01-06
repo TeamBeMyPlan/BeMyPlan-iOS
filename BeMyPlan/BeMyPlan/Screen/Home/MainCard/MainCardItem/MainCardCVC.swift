ddaf698db00a15dd9efe513fbc2263ddc41f0cbd
hellozo0 <ahju0011@gmail.com>
2022-01-06 23:56:43 +0900

    [feat]✨ : MainCardCVC 생성 (#13)

---
+++ BeMyPlan/BeMyPlan/Screen/Home/MainCard/MainCardItem/MainCardCVC.swift
@@ -0,0 +1,17 @@
+//
+//  MainCardCVC.swift
+//  BeMyPlan
+//
+//  Created by 안현주 on 2022/01/06.
+//
+
+import UIKit
+
+class MainCardCVC: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
