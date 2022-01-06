1d1b6a0db3466a507c7bf35d14ffe6f812801675
hellozo0 <ahju0011@gmail.com>
2022-01-06 18:26:39 +0900

    [feat]✨ MainCardView, MainListView xib 세팅 (#12)

---
+++ BeMyPlan/BeMyPlan/Screen/Home/MainList/MainListView.swift
@@ -0,0 +1,20 @@
+//
+//  MainListView.swift
+//  BeMyPlan
+//
+//  Created by 안현주 on 2022/01/06.
+//
+
+import UIKit
+
+class MainListView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
