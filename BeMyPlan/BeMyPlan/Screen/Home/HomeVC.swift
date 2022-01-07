ad25eb8fd95993a75bd9345b8a0b06ca06ccde19
hellozo0 <ahju0011@gmail.com>
2022-01-07 14:54:13 +0900

    [feat]✨ : Home의 상단 네비뷰의 그림자 효과 테스트(#13)

--- BeMyPlan/BeMyPlan/Screen/Home/HomeVC.swift
+++ BeMyPlan/BeMyPlan/Screen/Home/HomeVC.swift
@@ -8,23 +8,32 @@
 import UIKit
 
 class HomeVC: UIViewController {
-
+  
   // MARK: - Vars & Lets Part
-
+  
   // MARK: - UI Component Part
-
+  @IBOutlet var naviView: UIView!{
+    didSet {
+//      naviView.layer.shadowColor = UIColor(red: 0.796, green: 0.796, blue: 0.796, alpha: 0.25).cgColor
+//      naviView.layer.shadowOffset = CGSize(width: 1, height: 4)
+//      naviView.layer.shadowOpacity = 1
+//      naviView.layer.shadowRadius = 8
+//      naviView.layer.masksToBounds = false
+    }
+  }
+  
   // MARK: - Life Cycle Part
   
-    override func viewDidLoad() {
-        super.viewDidLoad()
-         
-    }
-    
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
   // MARK: - IBAction Part
-
+  
   // MARK: - Custom Method Part
 
+  
   // MARK: - @objc Function Part
-
+  
 }
 // MARK: - Extension Part
