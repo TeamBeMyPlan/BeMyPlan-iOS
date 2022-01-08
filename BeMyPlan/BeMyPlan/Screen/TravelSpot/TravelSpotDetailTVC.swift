3e7b67ebb04f9a38740fa68cdb8fc63da26789d9
i-colours-u <i.colours.u@gmail.com>
2022-01-08 16:29:33 +0900

    [fix] :hammer: merge all branches version

---
+++ BeMyPlan/BeMyPlan/Screen/TravelSpot/TravelSpotDetailTVC.swift
@@ -0,0 +1,30 @@
+//
+//  TravelSpotDetailTVC.swift
+//  BeMyPlan
+//
+//  Created by 조양원 on 2022/01/07.
+//
+
+import UIKit
+
+class TravelSpotDetailTVC: UITableViewCell {
+  
+  static let identifier = "TravelSpotDetailTVC"
+  
+  @IBOutlet var contentImage: UIImageView!
+  @IBOutlet var nickNameLabel: UILabel!
+  @IBOutlet var titleLabel: UILabel!
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+  }
+  
+  override func setSelected(_ selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
+  }
+  
+  override func layoutSubviews() {
+    super.layoutSubviews()
+  }
+  
+}
