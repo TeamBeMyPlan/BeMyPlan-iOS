7f7f60d99bbba06edcc8d1fffe7709abaf94cc32
hellozo0 <ahju0011@gmail.com>
2022-01-12 03:27:06 +0900

    [feat]✨ : LoginVC UI (#43)

--- BeMyPlan/BeMyPlan/Global/Supporting Files/SceneDelegate.swift
+++ BeMyPlan/BeMyPlan/Global/Supporting Files/SceneDelegate.swift
@@ -13,7 +13,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
 
     guard let _ = (scene as? UIWindowScene) else { return }
-    window!.rootViewController = UIStoryboard(name: "Base", bundle: nil).instantiateInitialViewController()!
+    window!.rootViewController = UIStoryboard(name: "Login", bundle: nil).instantiateInitialViewController()!
 
   }
 
--- BeMyPlan/BeMyPlan/Screen/Login/LoginVC.swift
+++ BeMyPlan/BeMyPlan/Screen/Login/LoginVC.swift
@@ -8,23 +8,27 @@
 import UIKit
 
 class LoginVC: UIViewController {
-
+  
   // MARK: - Vars & Lets Part
   
   // MARK: - UI Component Part
-
+  @IBOutlet var searchLabel: UILabel!
+  
+  
   // MARK: - Life Cycle Part
   
-    override func viewDidLoad() {
-        super.viewDidLoad()
-         
-    }
-    
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
   // MARK: - IBAction Part
-
+  @IBAction func touchUpToGoBaseView(_ sender: Any) {
+  }
+  
   // MARK: - Custom Method Part
 
+  
   // MARK: - @objc Function Part
-
+  
 }
 // MARK: - Extension Part
