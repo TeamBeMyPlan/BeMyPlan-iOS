6ed8043c5ba34c631d35127e6209be24d9b0b9c3
yangwin9616 <yangwon9616@naver.com>
2022-01-12 04:20:53 +0900

    [feat] Filter View Bottom Sheet 구현

--- BeMyPlan/BeMyPlan/Screen/TravelSpot/TravelSpotFilterVC.swift
+++ BeMyPlan/BeMyPlan/Screen/TravelSpot/TravelSpotFilterVC.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import PanModal
 
 class TravelSpotFilterVC: UIViewController {
   // MARK: - Vars & Lets Part
@@ -69,31 +70,34 @@ class TravelSpotFilterVC: UIViewController {
     //    })
   }
  
-  /*
-  private func setPanGesture() {
-    // UIPanGestureRecognizer는 target(ViewController)에서 drag가 감지되면 action을 실행한다.
-    let panGesture = UIPanGestureRecognizer(target: self, action: #selector(self.drag))
-    // panGesture가 보는 view는 circleView가 된다.
-    filterView.addGestureRecognizer(panGesture)
-  }
- */
   
-  /*
+  
+  
   // MARK: - @objc Function Part
-  @objc func drag(sender: UIPanGestureRecognizer) {
-    // self는 여기서 ViewController이므로 self.view ViewController가 기존에가지고 있는 view이다.
-    let translation = sender.translation(in: self.view) // translation에 움직인 위치를 저장한다.
-    
-    // sender의 view는 sender가 바라보고 있는 circleView이다. 드래그로 이동한 만큼 circleView를 이동시킨다.
-
-    filterViewBottomConstraints.constant = CGFloat(bottomValue)
-    // CGFloat(  // CGFloat(bottomValue)
-    bottomValue += 1
-  }
-  // 뷰를 눌렀을 때 화아아악 하고 투명도가 꽉 채워져야 하는데 그러면 backGroundView가 버튼이어야 하는가?
-   */
+  
 }
 // MARK: - Extension Part
+extension TravelSpotFilterVC: PanModalPresentable {
+  
+  var panScrollable: UIScrollView? {
+    return nil
+  }
+
+  // 처음 시작 위치
+  var shortFormHeight: PanModalHeight {
+    return .contentHeight(278)
+  }
+
+  
+  var longFormHeight: PanModalHeight {
+    return .maxHeightWithTopInset(278)
+  }
+  
+  var dragIndicatorBackgroundColor: UIColor {
+    return .clear
+  }
+}
+
 
 
 
