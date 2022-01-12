5f8ae96b79530670f3be8759f4c0f50331191cba
i-colours-u <i.colours.u@gmail.com>
2022-01-13 03:29:00 +0900

    [feat] :sparkles: 구매 후 뷰 테이블뷰 초안 완성

--- BeMyPlan/BeMyPlan/Screen/Plan/PlanDetail/PlanDetailVC.swift
+++ BeMyPlan/BeMyPlan/Screen/Plan/PlanDetail/PlanDetailVC.swift
@@ -10,6 +10,31 @@ import UIKit
 class PlanDetailVC: UIViewController {
 
   // MARK: - Vars & Lets Part
+  
+  var currentDay : Int = 1
+  var spotDataList : [PlanDetailData.SpotData] = [
+    PlanDetailData.SpotData.init(locationTitle: "산방산",
+                                 address
+                                 : "제주특별자치도 제주시 애월읍 유수암리 산138",
+                                 imagerUrls: [],
+                                 textContent: "테스트",
+                                 nextLocationData: PlanDetailData.Summary.init(transportCase: .walk, locationName: "다음장소", time: "5분")),
+    
+    PlanDetailData.SpotData.init(locationTitle: "산방2",
+                                 address
+                                 : "제주특별자치도 제주시 애월읍 유수암리 산138",
+                                 imagerUrls: [],
+                                 textContent: "테스트",
+                                 nextLocationData: PlanDetailData.Summary.init(transportCase: .walk, locationName: "다음장소", time: "5분")),
+    
+    PlanDetailData.SpotData.init(locationTitle: "산방3",
+                                 address
+                                 : "제주특별자치도 제주시 애월읍 유수암리 산138",
+                                 imagerUrls: [],
+                                 textContent: "테스트",
+                                 nextLocationData: PlanDetailData.Summary.init(transportCase: .walk, locationName: "다음장소", time: "5분"))
+  
+  ]
 
   // MARK: - UI Components Part
   
@@ -45,11 +70,40 @@ extension PlanDetailVC : UITableViewDelegate{
 
 extension PlanDetailVC : UITableViewDataSource{
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 5
+    return 4 + spotDataList.count
   }
   
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    return UITableViewCell()
+    
+    switch(indexPath.row){
+      case 0:
+        guard let writerCell = tableView.dequeueReusableCell(withIdentifier: PlanPreviewWriterTVC.className, for: indexPath) as? PlanPreviewWriterTVC else {return UITableViewCell() }
+        
+        writerCell.setHeaderData(author: "혜화동불가마", title: "좋은 여행지 소개 합니다 ^^")
+        
+        return writerCell
+      case 1:
+        guard let mapCell = tableView.dequeueReusableCell(withIdentifier: PlanDetailMapContainerTVC.className, for: indexPath) as? PlanDetailMapContainerTVC else {return UITableViewCell() }
+        
+        
+        return mapCell
+      case 2:
+        guard let selectDayCell = tableView.dequeueReusableCell(withIdentifier: PlanDetailSelectDayTVC.className, for: indexPath) as? PlanDetailSelectDayTVC else {return UITableViewCell() }
+        
+        selectDayCell.totalDay = 3
+        return selectDayCell
+      case 3:
+        guard let summaryCell = tableView.dequeueReusableCell(withIdentifier: PlanDetailSummaryTVC.className, for: indexPath) as? PlanDetailSummaryTVC else {return UITableViewCell() }
+        summaryCell.setDa
+        
+        return summaryCell
+      default :
+        guard let infoCell = tableView.dequeueReusableCell(withIdentifier: PlanDetailInformationTVC.className, for: indexPath) as? PlanDetailInformationTVC else {return UITableViewCell() }
+        infoCe
+        
+        return infoCell
+    }
+    
   }
   
   
