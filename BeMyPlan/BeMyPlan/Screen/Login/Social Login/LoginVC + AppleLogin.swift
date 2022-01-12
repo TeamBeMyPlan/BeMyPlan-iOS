10acc5aff20110bf2567a34b8bc8ef7bcf7be11d
hellozo0 <ahju0011@gmail.com>
2022-01-12 16:48:12 +0900

    [feat] ✨: Kakao Login file setting (#32)

---
+++ BeMyPlan/BeMyPlan/Screen/Login/Social Login/LoginVC + AppleLogin.swift
@@ -0,0 +1,8 @@
+//
+//  LoginVC + AppleLogin.swift
+//  BeMyPlan
+//
+//  Created by 안현주 on 2022/01/12.
+//
+
+import Foundation
---
+++ BeMyPlan/BeMyPlan/Screen/Login/Social Login/LoginVC + KakaoLogin.swift
@@ -0,0 +1,12 @@
+//
+//  LoginVC + KakaoLogin.swift
+//  BeMyPlan
+//
+//  Created by 안현주 on 2022/01/12.
+//
+
+import Foundation
+
+extension LoginVC {
+  
+}
