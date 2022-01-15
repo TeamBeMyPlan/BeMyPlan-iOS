d5622f82e5d4609fba02af426ab08da86d2cb29c
i-colours-u <i.colours.u@gmail.com>
2022-01-15 16:25:24 +0900

    [feat] :sparkles: 스플래시 완성 (#44)

--- BeMyPlan/BeMyPlan/Screen/Splash/SplashVC.swift
+++ BeMyPlan/BeMyPlan/Screen/Splash/SplashVC.swift
@@ -8,23 +8,48 @@
 import UIKit
 
 class SplashVC: UIViewController {
-
+  
   // MARK: - Vars & Lets Part
-
+  
   // MARK: - UI Component Part
-
+  @IBOutlet var splashIconNoTitle: UIImageView!
+  @IBOutlet var splashIcon: UIImageView!
+  
   // MARK: - Life Cycle Part
   
-    override func viewDidLoad() {
-        super.viewDidLoad()
-         
-    }
-    
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    startSplash()
+  }
+  
   // MARK: - IBAction Part
-
+  
   // MARK: - Custom Method Part
-
+  
+  private func startSplash(){
+    UIView.animateKeyframes(withDuration: 3, delay: 0, options: .allowUserInteraction) {
+      UIView.addKeyframe(withRelativeStartTime: 1/6,
+                         relativeDuration: 1/3) {
+        self.splashIcon.alpha = 1
+      }
+      
+      UIView.addKeyframe(withRelativeStartTime: 5/6,
+                         relativeDuration: 1/6) {
+        self.splashIcon.alpha = 0
+        self.splashIconNoTitle.alpha = 0
+      }
+    } completion: { _ in
+      
+    }
+  }
+  
+  private func moveBaseVC(){
+    let baseVC = UIStoryboard.list(.base).instantiateInitialViewController()!
+    
+    self.navigationController?.pushViewController(baseVC, animated: false)
+  }
+  
   // MARK: - @objc Function Part
-
+  
 }
 // MARK: - Extension Part
