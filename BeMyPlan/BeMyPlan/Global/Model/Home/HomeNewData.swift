c9cc90b7ede61d3bf857a85fb03fcbe7f3432461
hellozo0 <ahju0011@gmail.com>
2022-01-18 00:12:50 +0900

    [feat] ✨ New DataModel (#48)

---
+++ BeMyPlan/BeMyPlan/Global/Model/Home/HomeNewData.swift
@@ -0,0 +1,17 @@
+//
+//  HomeNewData.swift
+//  BeMyPlan
+//
+//  Created by 안현주 on 2022/01/18.
+//
+
+import Foundation
+
+extension HomeNewData{
+  struct NewList{
+    var id : Int
+    var title : String
+    var photo : String
+    var nickname : String
+  }
+}
