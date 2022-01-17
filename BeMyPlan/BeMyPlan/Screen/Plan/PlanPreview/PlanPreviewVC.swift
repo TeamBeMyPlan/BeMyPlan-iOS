484d067072fdc7fb40dce30aaef630f276d402f0
i-colours-u <i.colours.u@gmail.com>
2022-01-18 06:32:38 +0900

    [fix] :hammer: baseVC tabbar fadein/out fixed (#47)

--- BeMyPlan/BeMyPlan/Screen/Plan/PlanPreview/PlanPreviewVC.swift
+++ BeMyPlan/BeMyPlan/Screen/Plan/PlanPreview/PlanPreviewVC.swift
@@ -53,6 +53,8 @@ class PlanPreviewVC: UIViewController {
     fetchDummyData()
     setScrabImage()
     addButtonActions()
+    fetchTagData()
+    fetchDetailData()
   }
   @IBAction func backButtonClicked(_ sender: Any) {
     self.navigationController?.popViewController(animated: true)
@@ -116,7 +118,6 @@ class PlanPreviewVC: UIViewController {
       result.success { [weak self] data in
         if let data = data{
           var photoList : [PlanPreview.PhotoData] = []
-          
           for (_,item) in data.enumerated(){
             photoList.append(PlanPreview.PhotoData.init(photo: item.photoURL,
                                                         content: item.description))
@@ -175,7 +176,6 @@ extension PlanPreviewVC : UITableViewDataSource{
       case .recommend:
         guard let recommendCell = tableView.dequeueReusableCell(withIdentifier: PlanPreviewRecommendTVC.className, for: indexPath) as? PlanPreviewRecommendTVC else {return UITableViewCell() }
         
-        recommendCell.setRecommendData(title: recommendData?.title, content: recommendData?.content)
         
         return recommendCell
     }
@@ -200,12 +200,7 @@ extension PlanPreviewVC{
                                                                                 budget: "45만원",
                                                                                 transport: "버스",
                                                                                 month: "3달"))
-    photoData = [
-      PlanPreview.PhotoData(photo: "https://picsum.photos/id/1/300/300", content: "친구와 함께 다녀온 제주 힐링 여행을 콘텐츠로 담아봤어요."),
-      PlanPreview.PhotoData(photo: "https://picsum.photos/id/1/300/300", content: "저는 평소에 국내 여행을 다닐 때 대표 관광지나 사람이 너무 많은 핫플보다는,여유를 느낄 수 있는 공간들을 선호하는 편이에요!"),
-      PlanPreview.PhotoData(photo: "https://picsum.photos/id/1/300/300", content: "이번 여행 일정 콘텐츠에서도 자연과 감성을 담은 곳들 위주로 일정을 짜고 여행을 다녀왔어요."),
-      PlanPreview.PhotoData(photo: "https://picsum.photos/id/1/300/300", content: "저와 비슷한 취향을 가지신 분들이라면 제 일정대로 따라가보세요😎"),
-    ]
+
     summaryData = PlanPreview.SummaryData(content: "여유로운 3박 4일 일정이었는데 타이트하게 잡는다면 꽉 채운 2박 3일도 가능할 것 같은 일정이에요 ㅎㅎ 자연 보면서 힐링 할 수 있는 여행지와 감성가득한 카페, 맛있는 식사만 있어도 여행은 충분하지 ! 라고 생각하시는 분이라면 제 일정대로 따라가보시면 좋을 것 같습니다 :)")
     recommendData = PlanPreview.RecommendData()
     setContentList()
@@ -215,7 +210,7 @@ extension PlanPreviewVC{
 extension PlanPreviewVC : UIScrollViewDelegate{
   func scrollViewDidScroll(_ scrollView: UIScrollView) {
     
-    if lastContentOffset > scrollView.contentOffset.y && lastContentOffset < scrollView.contentSize.height - scrollView.frame.height {
+    if lastContentOffset > scrollView.contentOffset.y && lastContentOffset - 40 < scrollView.contentSize.height - scrollView.frame.height {
       moveBuyContainer(state: .show)
     } else if lastContentOffset < scrollView.contentOffset.y && scrollView.contentOffset.y > 0 {
       
