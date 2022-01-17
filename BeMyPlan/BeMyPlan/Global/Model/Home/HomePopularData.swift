0095fa429156272def04a0c08f05b37792c01262
hellozo0 <ahju0011@gmail.com>
2022-01-18 00:11:13 +0900

    [feat] ✨ Popular DataModel (#48)

---
+++ BeMyPlan/BeMyPlan/Global/Model/Home/HomePopularData.swift
@@ -0,0 +1,16 @@
+//
+//  HomePopularData.swift
+//  BeMyPlan
+//
+//  Created by 안현주 on 2022/01/18.
+//
+
+import Foundation
+
+extension HomePopularData{
+  struct PopularList{
+    var id : Int
+    var title : String
+    var photo : String
+  }
+}
