70d0616bdb8660941bc3f90eca578efc7026c56d
i-colours-u <i.colours.u@gmail.com>
2022-02-02 03:56:58 +0900

    [feat] :sparkles: ModuleFactory 추가
    VC 인스턴스를 만들기 위한 ModuleFactory 추가 완료

--- BeMyPlan/BeMyPlan/Coordinator/Factory/ModuleFactory.swift
+++ BeMyPlan/BeMyPlan/Coordinator/Factory/ModuleFactory.swift
@@ -35,7 +35,85 @@ protocol ModuleFactroyProtocol {
   func instantiatePaymentSelectVC() -> PaymentSelectVC
   func instantiatePaymentCompleteVC() -> PaymentCompleteVC
   
+  // MARK: - Plan
+  func instantiatePlanPreviewVC() -> PlanPreviewVC
+  func instantiatePlanDetailVC() -> PlanDetailVC
   
+  // MARK: - PlanList
+  func instantiatePlanListVC() -> TravelSpotDetailVC
+}
+
+class ModuleFactory: ModuleFactroyProtocol{
+
+  static func resolve() -> ModuleFactory {
+    return ModuleFactory()
+  }
   
+  // MARK: - Splash
+  func instantiateSplashVC() -> SplashVC {
+    return SplashVC.controllerFromStoryboard(.splash)
+  }
+  
+  // MARK: - Auth
+  func instantiateLoginVC() -> LoginVC {
+    return LoginVC.controllerFromStoryboard(.login)
+  }
+  
+  func instantiateSignupVC() -> SignUpVC {
+    return SignUpVC.controllerFromStoryboard(.signup)
+  }
+  
+  // MARK: - Base Tap
+
+  func instantiateBaseVC() -> BaseVC {
+    return BaseVC.controllerFromStoryboard(.base)
+  }
+  
+  // MARK: - Home
+
+  func instantiateHomeVC() -> HomeVC {
+    return HomeVC.controllerFromStoryboard(.home)
+  }
+  
+  // MARK: - TravelSpot
+  
+  func instantitateTravelSpotVC() -> TravelSpotVC {
+    return TravelSpotVC.controllerFromStoryboard(.travelSpot)
+  }
+  
+  // MARK: - Scrap
+
+  func instantiateScrapVC() -> ScrapVC {
+    return ScrapVC.controllerFromStoryboard(.scrap)
+  }
+  
+  // MARK: - MyPlan
+  func instantiateMyPlanVC() -> MyPlanVC {
+    return MyPlanVC.controllerFromStoryboard(.myPlan)
+  }
+  
+  // MARK: - Payment
+
+  func instantiatePaymentSelectVC() -> PaymentSelectVC {
+    return PaymentSelectVC.controllerFromStoryboard(.payment)
+  }
+  
+  func instantiatePaymentCompleteVC() -> PaymentCompleteVC {
+    return PaymentCompleteVC.controllerFromStoryboard(.payment)
+  }
+  
+  // MARK: - Plan
+
+  func instantiatePlanPreviewVC() -> PlanPreviewVC {
+    return PlanPreviewVC.controllerFromStoryboard(.planPreview)
+  }
+  
+  func instantiatePlanDetailVC() -> PlanDetailVC {
+    return PlanDetailVC.controllerFromStoryboard(.planDetail)
+  }
+  
+  func instantiatePlanListVC() -> TravelSpotDetailVC {
+    return TravelSpotDetailVC.controllerFromStoryboard(.travelSpot)
+  }
   
 }
