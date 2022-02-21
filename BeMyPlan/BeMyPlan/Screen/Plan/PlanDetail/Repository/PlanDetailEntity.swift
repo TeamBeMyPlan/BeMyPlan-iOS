913f7e2e9d6e3ccec2ba56fe5586ea3d3ed588d8
i-colours-u <i.colours.u@gmail.com>
2022-02-22 03:42:15 +0900

    [refactor] :recycle: planDetail Repository 생성 (#113)

---
+++ BeMyPlan/BeMyPlan/Screen/Plan/PlanDetail/Repository/PlanDetailEntity.swift
@@ -0,0 +1,8 @@
+//
+//  PlanDetailEntity.swift
+//  BeMyPlan
+//
+//  Created by 송지훈 on 2022/02/22.
+//
+
+import Foundation
---
+++ BeMyPlan/BeMyPlan/Screen/Plan/PlanDetail/Repository/PlanDetailRepository.swift
@@ -0,0 +1,42 @@
+//
+//  PlanDetailRepository.swift
+//  BeMyPlan
+//
+//  Created by 송지훈 on 2022/02/22.
+//
+
+import Foundation
+
+protocol PlanDetailRepositoryInterface {
+  var networkError: ((Error) -> Void)? { get set }
+  func fetchPlanDetailData(idx: Int,onCompleted: @escaping (String,String,Int, [[PlanDetailDataGettable.SpotData?]]) -> Void)
+}
+
+final class PlanDetailRepository: PlanDetailRepositoryInterface {
+
+  // MARK: - Outputs
+  var networkError: ((Error) -> Void)?
+  
+  private let networkService: PlanDetailServiceType
+  
+  init(service: PlanDetailServiceType) {
+    self.networkService = service
+  }
+  
+  func fetchPlanDetailData(idx: Int, onCompleted: @escaping (String,String,Int, [[PlanDetailDataGettable.SpotData?]]) -> Void) {
+    networkService.getPlanDetailData(idx: idx) { [weak self] result in
+      guard let self = self else {return}
+      result.success { entity in
+        guard let entity = entity else {return}
+        let title = entity.title
+        let writer = entity.author
+        let totalDays = entity.totalDays
+        let spots = entity.spots
+        
+        onCompleted(title,writer,totalDays,spots)
+      }.catch { error in
+        self.networkError?(error)
+      }
+    }
+  }
+}
