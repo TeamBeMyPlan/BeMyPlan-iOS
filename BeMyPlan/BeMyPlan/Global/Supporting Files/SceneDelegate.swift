fba269598485989fae1a182cba8a7fc6dd66a393
hellozo0 <ahju0011@gmail.com>
2022-02-28 16:24:14 +0900

    [feat] SignUpNickNameVC UI(#114)

--- BeMyPlan/BeMyPlan/Global/Supporting Files/SceneDelegate.swift
+++ BeMyPlan/BeMyPlan/Global/Supporting Files/SceneDelegate.swift
@@ -13,7 +13,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   var window: UIWindow?
   func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
     guard let _ = (scene as? UIWindowScene) else { return }
-    window!.rootViewController = UIStoryboard(name: "Splash", bundle: nil).instantiateInitialViewController()!
+    window!.rootViewController = UIStoryboard(name: "SignUp", bundle: nil).instantiateInitialViewController()!
   }
   func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
     if let url = URLContexts.first?.url {
