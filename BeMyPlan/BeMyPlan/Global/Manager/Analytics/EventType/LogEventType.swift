8dad205000ba187ac9b6182a686ec644883132ab
i-colours-u <i.colours.u@gmail.com>
2022-03-03 18:41:04 +0900

    [feat] AppLog Case Params 정의 완료 (#117)

--- BeMyPlan/BeMyPlan/Global/Manager/Analytics/EventType/LogEventType.swift
+++ BeMyPlan/BeMyPlan/Global/Manager/Analytics/EventType/LogEventType.swift
@@ -7,44 +7,6 @@
 
 import Foundation
 
-/**
- 앱 설치 --> appFirstOpen
- 앱 삭제 --> 추적 불가능
- 
- 로그인
- 
- 
- 스크랩 눌러서 로그인
- 구매하기 눌러서 로그인
- 마이플랜에서 로그인
- 
- --> login + type + userID
- 
-  로그아웃
- --> logout + userID
- 
- 
- 
- 
- 
- 메인홈 콘텐츠 보기
- 여행지탭에 들어가 콘텐츠 리스트 보기
- 콘텐츠 선택
- 스크랩한 여행 일정 보기
- -->  clickPlanDetail + case + postID
-
- 
- 여행 일정 스크랩 하기
- 여행 일정 스크랩 제거
- --> scrap + postID
- 
- 
- 구매하기 누름
- 결제수단 선택
- 결제하기 누름
- 마이플랜에서 구매한 여행 일정 누름
-
- */
 enum LogEventType {
   // 1. 앱 최초 실행
   case appFirstOpen
@@ -88,86 +50,63 @@ enum LogEventType {
   // 11.결제 뷰
   case closePaymentView
   case clickPaymentMethod(source: PaymentSource)
+  case clickPaymentButton(postIdx: String)
   
   // 12. 결제 완료 뷰
   case closePaymentCompleteView
-  case clickPlanDetailView(postIdx: String)
+  case clickPlanDetailViewInPayment(postIdx: String)
   
   // 13. 결제 전 여행일정
   case clickPlanDetailExample
   
   // 14. 결제 후 여행일정
   case clickAddressCopy(postIdx: String)
-  case moveMapApplication(source: MapSource)
+  case moveMapApplication(source: MapSource,postIdx: String)
   case alertNoMapApplication
   
   // 15. 작성자 이름 클릭
-  case clickEditorName(source: ViewSource)
+  case clickEditorName(source: ViewSource,postIdx: String)
   
   // 16. 회원 유입 및 이탈
   case enterForeGround
   case enterBackGround
   case logout
   case withdrawal
-  
-
 }
 
 extension LogEventType: EventType {
   func name(for provider: ProviderType) -> String? {
     switch self {
-      case .appFirstOpen: return "event_firebase_first_open"
-      case .onboardingFirstOpen: return "event_onboarding_first_open"
-      case .onboardingEnd: return "event_onboarding_complete"
-      case .signin: return "event_signin_click"
-      case .signupNickname: return "event_signup_nickname"
-      case .signupEmail: return "onboarding_first_open"
-      case .signupComplete: return "onboarding_first_open"
-      case .clickTab: return "onboarding_first_open"
-      case .clickHomeRecentPlanList: return "onboarding_first_open"
-      case .clickHomeRecommendPlanList: return "onboarding_first_open"
-      case .clickOpenedTravelSpot(spot: let spot): return "onboarding_first_open"
-        
-      case .clickClosedTravelSpot(spot: let spot): return "onboarding_first_open"
-        
-      case .showLoginPage(source: let source):
-        return "onboarding_first_open"
-        
-      case .clickTravelPlan(source: let source, postIdx: let postIdx):
-        return "onboarding_first_open"
-        
-      case .scrapTravelPlan(source: let source, postIdx: let postIdx):
-        return "onboarding_first_open"
-        
-      case .scrapCancelTravelPlan(source: let source, postIdx: let postIdx):
-        return "onboarding_first_open"
-        
-      case .closePaymentView: return "onboarding_first_open"
-        
-      case .clickPaymentMethod(source: let source): return "onboarding_first_open"
-        
-      case .closePaymentCompleteView: return "onboarding_first_open"
-        
-      case .clickPlanDetailView(postIdx: let postIdx): return "onboarding_first_open"
-        
-      case .clickPlanDetailExample: return "onboarding_first_open"
-        
-      case .clickAddressCopy(postIdx: let postIdx): return "onboarding_first_open"
-        
-      case .moveMapApplication(source: let source): return "onboarding_first_open"
-        
-      case .alertNoMapApplication: return "onboarding_first_open"
-        
-      case .clickEditorName(source: let source): return "onboarding_first_open"
-        
-      case .enterForeGround: return "onboarding_first_open"
-        
-      case .enterBackGround: return "onboarding_first_open"
-        
-      case .logout: return "onboarding_first_open"
-        
-      case .withdrawal: return "onboarding_first_open"
-        
+      case .appFirstOpen:                 return "firebase_first_open"
+      case .onboardingFirstOpen:          return "onboarding_first_open"
+      case .onboardingEnd:                return "onboarding_complete"
+      case .signin:                       return "signin_click"
+      case .signupNickname:               return "signup_nickname"
+      case .signupEmail:                  return "signup_email"
+      case .signupComplete:               return "signup_complete"
+      case .clickTab:                     return "click_tab"
+      case .clickHomeRecentPlanList:      return "click_home_recent_list"
+      case .clickHomeRecommendPlanList:   return "click_home_recommend_list"
+      case .clickOpenedTravelSpot:        return "click_opened_travel_spot"
+      case .clickClosedTravelSpot:        return "click_closed_travel_spot"
+      case .showLoginPage:                return "move_login_page"
+      case .clickTravelPlan:              return "click_travel_plan"
+      case .scrapTravelPlan:              return "scrap_travel_plan"
+      case .scrapCancelTravelPlan:        return "scrap_cancel_travel_plan"
+      case .closePaymentView:             return "close_payment_view"
+      case .clickPaymentMethod:           return "click_payment_method"
+      case .clickPaymentButton:           return "click_payment_button"
+      case .closePaymentCompleteView:     return "close_payment_complete"
+      case .clickPlanDetailViewInPayment: return "move_plan_detail_view_in_payment"
+      case .clickPlanDetailExample:       return "click_plan_detail_example"
+      case .clickAddressCopy:             return "click_address_copy"
+      case .moveMapApplication:           return "move_map_application"
+      case .alertNoMapApplication:        return "alert_no_map_application"
+      case .clickEditorName:              return "click_editor_name"
+      case .enterForeGround:              return "enter_foreground"
+      case .enterBackGround:              return "enter_background"
+      case .logout:                       return "user_logout"
+      case .withdrawal:                   return "user_withdrawal"
     }
   }
   
@@ -177,6 +116,46 @@ extension LogEventType: EventType {
       "userIdx": String(UserDefaults.standard.integer(forKey: "userIdx"))
     ]
     switch self {
+      case .signin(let loginSource),
+          .signupNickname(let loginSource),
+          .signupEmail(let loginSource),
+          .signupComplete(let loginSource):
+        params["loginSource"] = loginSource.rawValue
+        
+      case .clickTab(let tabSource):
+        params["tabSource"] = tabSource.rawValue
+      
+      case .clickOpenedTravelSpot(let spot),
+          .clickClosedTravelSpot(let spot):
+        params["spot"] = spot
+    
+      case .showLoginPage(let viewSource):
+        params["viewSource"] = viewSource.rawValue
+        
+      case .clickTravelPlan(let viewSource, let postIdx):
+        params["viewSource"] = viewSource.rawValue
+        params["postIdx"] = postIdx
+        
+      case .scrapTravelPlan(let viewSource, let postIdx),
+          .scrapCancelTravelPlan(let viewSource, let postIdx):
+        params["viewSource"] = viewSource.rawValue
+        params["postIdx"] = postIdx
+        
+      case .clickPaymentMethod(let paymentSource):
+        params["paymentSource"] = paymentSource.rawValue
+      
+      case .clickPaymentButton(let postIdx),
+          .clickPlanDetailViewInPayment(let postIdx),
+          .clickAddressCopy(let postIdx):
+        params["postIdx"] = postIdx
+        
+      case .moveMapApplication(let mapSource, let postIdx):
+        params["mapSource"] = mapSource
+        params["postIdx"] = postIdx
+
+      case .clickEditorName(let viewSource, let postIdx):
+        params["viewSource"] = viewSource
+        params["postIdx"] = postIdx
 
       default: break
     }
