0a3db7bd544861149ab96c0097ac893f1b873c62
i-colours-u <i.colours.u@gmail.com>
2022-03-03 17:48:33 +0900

    [feat] 이벤트 log enum case 정의 (#117)
    앱 내에서 저장할 로그 타입을 enum으로 모두 정의합니다.

--- BeMyPlan/BeMyPlan/Global/Manager/Analytics/EventType/LogEventType.swift
+++ BeMyPlan/BeMyPlan/Global/Manager/Analytics/EventType/LogEventType.swift
@@ -12,23 +12,31 @@ import Foundation
  앱 삭제 --> 추적 불가능
  
  로그인
+ 
+ 
  스크랩 눌러서 로그인
  구매하기 눌러서 로그인
  마이플랜에서 로그인
  
- --> login + type
+ --> login + type + userID
+ 
+  로그아웃
+ --> logout + userID
+ 
+ 
+ 
  
  
  메인홈 콘텐츠 보기
  여행지탭에 들어가 콘텐츠 리스트 보기
  콘텐츠 선택
  스크랩한 여행 일정 보기
- -->  clickPlanDetail + case
+ -->  clickPlanDetail + case + postID
 
  
  여행 일정 스크랩 하기
  여행 일정 스크랩 제거
- --> scrap
+ --> scrap + postID
  
  
  구매하기 누름
@@ -38,54 +46,128 @@ import Foundation
 
  */
 enum LogEventType {
+  // 1. 앱 최초 실행
   case appFirstOpen
-  case signUp
-  case clickHomePlanDetail
-  case clickSpotList
-  case clickPlanDetailInSpotList
   
+  // 2. 온보딩
+  case onboardingFirstOpen
+  case onboardingEnd
   
+  // 3. 로그인
+  case signin(source: LoginSource)
   
+  // 4. 회원가입
+  case signupNickname(source: LoginSource)
+  case signupEmail(source: LoginSource)
+  case signupComplete(source: LoginSource)
   
-  case addSubjectTimetable(subjectIdx: String, subjectName: String)
-  case enterCommunicationRoom(subjectIdx: String)
-  case sendCommunicationMessage(subjectIdx: String, messageType: String)
-  case loadCommunityArticle(postIdx: String)
-  case writeCommunityArticle(boardIdx: String)
-  case writeCommunityArticleComment(postIdx: String)
-  case loadCommunityQnA(qnaIdx: String)
-  case writeCommunityQnA(tagIdx: String)
-  case writeCommunityQnaAnswer(qnaIdx: String)
-  case clickBottomTabHome
-  case clickBottomTabClass
-  case clickBottomTabCommunity
-  case clickBottomTabQna
-  case clickBottomTabMypage
-  case clickHomeMajorDept
-  case clickCommunityMajorDept
+  // 5. 탭바 클릭 액션
+  case clickTab(source: TabSource)
+  
+  // 6. 홈탭 내 액션
+  case clickHomeRecentPlanList
+  case clickHomeRecommendPlanList
+  
+  // 7. 여행지 뷰
+  case clickOpenedTravelSpot(spot: String)
+  case clickClosedTravelSpot(spot: String)
+  
+  // 8. 둘러보기 -> 로그인뷰 띄워지는 경우
+  case showLoginPage(source: ViewSource)
+
+  // 9. 여행 일정 클릭
+  case clickTravelPlan(source :ViewSource,
+                       postIdx: String)
+  
+  // 10. 스크랩 액션
+  case scrapTravelPlan(source: ViewSource,
+                       postIdx: String)
+  case scrapCancelTravelPlan(source: ViewSource,
+                             postIdx: String)
+  
+  // 11.결제 뷰
+  case closePaymentView
+  case clickPaymentMethod(source: PaymentSource)
+  
+  // 12. 결제 완료 뷰
+  case closePaymentCompleteView
+  case clickPlanDetailView(postIdx: String)
+  
+  // 13. 결제 전 여행일정
+  case clickPlanDetailExample
+  
+  // 14. 결제 후 여행일정
+  case clickAddressCopy(postIdx: String)
+  case moveMapApplication(source: MapSource)
+  case alertNoMapApplication
+  
+  // 15. 작성자 이름 클릭
+  case clickEditorName(source: ViewSource)
+  
+  // 16. 회원 유입 및 이탈
+  case enterForeGround
+  case enterBackGround
+  case logout
+  case withdrawal
+  
+
 }
 
 extension LogEventType: EventType {
   func name(for provider: ProviderType) -> String? {
     switch self {
-      case .appFirstOpen: return "Appsflyer_app_open"
-      case .signUp: return "Appsflyer_sign_up"
-      case .addSubjectTimetable: return "timetable_add_subject"
-      case .enterCommunicationRoom: return "lecture_entrance"
-      case .sendCommunicationMessage: return "lecture_send_message"
-      case .loadCommunityArticle: return "community_article_load"
-      case .writeCommunityArticle: return "community_article_write"
-      case .writeCommunityArticleComment: return "community_article_comment_write"
-      case .loadCommunityQnA: return "community_qna_load"
-      case .writeCommunityQnA: return "community_qna_write"
-      case .writeCommunityQnaAnswer: return "community_qna_write_answer"
-      case .clickBottomTabHome: return "event_click_bottom_tab_home"
-      case .clickBottomTabClass: return "event_click_bottom_tab_class"
-      case .clickBottomTabCommunity: return "event_click_bottom_tab_community"
-      case .clickBottomTabQna: return "event_click_bottom_tab_qna"
-      case .clickBottomTabMypage: return "event_click_bottom_tab_mypage"
-      case .clickHomeMajorDept: return "event_click_home_major_dept"
-      case .clickCommunityMajorDept: return "event_click_community_major_dept"
+      case .appFirstOpen: return "event_firebase_first_open"
+      case .onboardingFirstOpen: return "event_onboarding_first_open"
+      case .onboardingEnd: return "event_onboarding_complete"
+      case .signin: return "event_signin_click"
+      case .signupNickname: return "event_signup_nickname"
+      case .signupEmail: return "onboarding_first_open"
+      case .signupComplete: return "onboarding_first_open"
+      case .clickTab: return "onboarding_first_open"
+      case .clickHomeRecentPlanList: return "onboarding_first_open"
+      case .clickHomeRecommendPlanList: return "onboarding_first_open"
+      case .clickOpenedTravelSpot(spot: let spot): return "onboarding_first_open"
+        
+      case .clickClosedTravelSpot(spot: let spot): return "onboarding_first_open"
+        
+      case .showLoginPage(source: let source):
+        return "onboarding_first_open"
+        
+      case .clickTravelPlan(source: let source, postIdx: let postIdx):
+        return "onboarding_first_open"
+        
+      case .scrapTravelPlan(source: let source, postIdx: let postIdx):
+        return "onboarding_first_open"
+        
+      case .scrapCancelTravelPlan(source: let source, postIdx: let postIdx):
+        return "onboarding_first_open"
+        
+      case .closePaymentView: return "onboarding_first_open"
+        
+      case .clickPaymentMethod(source: let source): return "onboarding_first_open"
+        
+      case .closePaymentCompleteView: return "onboarding_first_open"
+        
+      case .clickPlanDetailView(postIdx: let postIdx): return "onboarding_first_open"
+        
+      case .clickPlanDetailExample: return "onboarding_first_open"
+        
+      case .clickAddressCopy(postIdx: let postIdx): return "onboarding_first_open"
+        
+      case .moveMapApplication(source: let source): return "onboarding_first_open"
+        
+      case .alertNoMapApplication: return "onboarding_first_open"
+        
+      case .clickEditorName(source: let source): return "onboarding_first_open"
+        
+      case .enterForeGround: return "onboarding_first_open"
+        
+      case .enterBackGround: return "onboarding_first_open"
+        
+      case .logout: return "onboarding_first_open"
+        
+      case .withdrawal: return "onboarding_first_open"
+        
     }
   }
   
@@ -95,28 +177,44 @@ extension LogEventType: EventType {
       "userIdx": String(UserDefaults.standard.integer(forKey: "userIdx"))
     ]
     switch self {
-      case let .addSubjectTimetable(subjectIdx, subjectName):
-        params["subjectIdx"] = subjectIdx
-        params["subjectName"] = subjectName
-      case let .enterCommunicationRoom(subjectIdx):
-        params["subjectIdx"] = subjectIdx
-      case let .sendCommunicationMessage(subjectIdx, messageType):
-        params["subjectIdx"] = subjectIdx
-        params["messageType"] = messageType
-      case let .loadCommunityArticle(postIdx):
-        params["postIdx"] = postIdx
-      case let .writeCommunityArticle(boardIdx):
-        params["boardIdx"] = boardIdx
-      case let .writeCommunityArticleComment(postIdx):
-        params["postIdx"] = postIdx
-      case let .loadCommunityQnA(qnaIdx):
-        params["qnaIdx"] = qnaIdx
-      case let .writeCommunityQnA(tagIdx):
-        params["tagIdx"] = tagIdx
-      case let .writeCommunityQnaAnswer(qnaIdx):
-        params["qnaIdx"] = qnaIdx
+
       default: break
     }
     return params
+  }
+}
+
+
+extension LogEventType {
+  enum LoginSource: String {
+    case kakao = "kakao"
+    case apple = "apple"
+    case guest = "guest"
+  }
+  
+  enum TabSource: String {
+    case home = "home"
+    case travelSpot = "travelSpot"
+    case scrap = "scrap"
+    case myPlan = "myPlan"
+  }
+  
+  enum ViewSource: String {
+    case scrapView = "scrapView"
+    case travelListView = "travelListView"
+    case planPreview = "planPreview"
+    case PlanDetail = "planDetail"
+    case myPlanView = "myPlanView"
+  }
+  
+  enum PaymentSource: String{
+    case kakaoPay = "kakaoPay"
+    case naverPay = "naverPay"
+    case toss = "toss"
+  }
+  
+  enum MapSource: String{
+    case kakaoMap = "kakaoMap"
+    case naverMap = "naverMap"
   }
 }
