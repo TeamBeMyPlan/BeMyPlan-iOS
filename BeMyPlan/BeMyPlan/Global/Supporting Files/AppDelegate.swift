9df6bd4badb644fa41a67b955031b6ce0af5bebc
i-colours-u <i.colours.u@gmail.com>
2022-03-03 16:13:06 +0900

    [feat] AppLog 클래스 추가 (#117)

--- BeMyPlan/BeMyPlan/Global/Supporting Files/AppDelegate.swift
+++ BeMyPlan/BeMyPlan/Global/Supporting Files/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
-    FirebaseApp.configure()
+    setupFirebaseSDK()
     KakaoSDK.initSDK(appKey: "904d52aff326e5526e945eee5dfb8efd")
     return true
   }
@@ -36,5 +36,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // Called when the user discards a scene session.
     // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
     // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+  }
+}
+
+extension AppDelegate{
+  func setupFirebaseSDK() {
+    AppLog.configure()
   }
 }
