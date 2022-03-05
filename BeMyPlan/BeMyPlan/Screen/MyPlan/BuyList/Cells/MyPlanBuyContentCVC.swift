7e6bab328eac69544c5991d684fc4773ded35897
i-colours-u <i.colours.u@gmail.com>
2022-03-06 01:34:13 +0900

    [chore] MyPlanBuyContentCVC 빈칸 수정

--- BeMyPlan/BeMyPlan/Screen/MyPlan/BuyList/Cells/MyPlanBuyContentCVC.swift
+++ BeMyPlan/BeMyPlan/Screen/MyPlan/BuyList/Cells/MyPlanBuyContentCVC.swift
@@ -8,19 +8,21 @@
 import UIKit
 
 class MyPlanBuyContentCVC: UICollectionViewCell {
-  @IBOutlet var contentImageView : UIImageView!
-  @IBOutlet var titleLabel : UILabel!
   
-  @IBOutlet var scrapImageView: UIImageView!
-  var isScrap = false {
-    didSet{
-      setScrapImage()
-    }
-  }
+  var isScrap = false { didSet{ setScrapImage() }}
   var postIdx: Int = 0
-  
   var scrapClicked: ((Bool,Int) -> (Void))?
   
+  @IBOutlet var contentImageView : UIImageView!
+  @IBOutlet var titleLabel : UILabel!
+  @IBOutlet var scrapImageView: UIImageView!
+  
+  @IBAction func scrapButtonClicked(_ sender: Any) {
+    makeVibrate()
+    isScrap.toggle()
+    scrapClicked?(isScrap,postIdx)
+  }
+
   func setContentData(title : String, imageURL: String,isScrap: Bool,postIdx: Int){
     contentImageView.layer.cornerRadius = 5
     titleLabel.text = title
@@ -34,11 +36,4 @@ class MyPlanBuyContentCVC: UICollectionViewCell {
     isScrap ? (image = ImageLiterals.Scrap.scrapFIconFilled) : (image = ImageLiterals.Scrap.scrapIconNotFilled)
     scrapImageView.image = image
   }
-  
-  @IBAction func scrapButtonClicked(_ sender: Any) {
-    makeVibrate()
-    isScrap.toggle()
-    scrapClicked?(isScrap,postIdx)
-  }
-  
 }
