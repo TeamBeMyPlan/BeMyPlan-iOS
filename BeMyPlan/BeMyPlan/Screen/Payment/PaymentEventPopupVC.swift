d18ce698952b0c85fc100c561f4be47e564c2526
i-colours-u <i.colours.u@gmail.com>
2022-03-05 21:57:14 +0900

    [feat] 이벤트 팝업 뷰 추가(#116)

---
+++ BeMyPlan/BeMyPlan/Screen/Payment/PaymentEventPopupVC.swift
@@ -0,0 +1,34 @@
+//
+//  PaymentEventPopupVC.swift
+//  BeMyPlan
+//
+//  Created by 송지훈 on 2022/03/05.
+//
+
+import UIKit
+
+class PaymentEventPopupVC: UIViewController {
+
+  // MARK: - Vars & Lets Part
+
+
+  // MARK: - UI Component Part
+
+  
+  // MARK: - Life Cycle Part
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+         
+    }
+    
+  // MARK: - IBAction Part
+
+  
+  // MARK: - Custom Method Part
+
+  
+  // MARK: - @objc Function Part
+
+}
+// MARK: - Extension Part
