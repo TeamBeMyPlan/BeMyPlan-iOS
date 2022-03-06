d04e5ca4deae6e5aa64b93147198876c31a971eb
i-colours-u <i.colours.u@gmail.com>
2022-03-07 03:32:53 +0900

    [feat] ViewModelType protocol 추가

--- BeMyPlan/BeMyPlan/Global/Protocol/ViewModelType.swift
+++ BeMyPlan/BeMyPlan/Global/Protocol/ViewModelType.swift
@@ -6,7 +6,13 @@
 //
 
 import Foundation
+import RxSwift
 
 protocol ViewModelType{
+  associatedtype Input
+  associatedtype Output
   
+  var disposeBag: DisposeBag { get set }
+  
+  func transform(input: Input) -> Output
 }
