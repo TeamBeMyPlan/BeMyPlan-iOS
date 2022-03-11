ca5b8d8d3148debbd654ed3e604c38ede88f75b3
i-colours-u <i.colours.u@gmail.com>
2022-03-11 12:57:04 +0900

    [feat] onboarding VC에서 기본적 UI 컴퍼넌트 추가

--- BeMyPlan/BeMyPlan/Screen/Onboarding/OnboardingVC.swift
+++ BeMyPlan/BeMyPlan/Screen/Onboarding/OnboardingVC.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Then
 
 class OnboardingVC: UIViewController {
 
@@ -13,8 +14,39 @@ class OnboardingVC: UIViewController {
 
 
   // MARK: - UI Component Part
+  
+  private lazy var mainContentView = UIView()
+  private lazy var bottomPageControlView = UIView()
+  private lazy var nextActionButton = UIButton()
+  private lazy var skipActionButton = UIButton()
 
   
+  
+  private lazy var headerFirstLabel = UILabel().then {
+    $0.setTextWithLineHeight(text: "여행 취향이 비슷한 크리에이터의 \n여행 일정을 찾아보세요",
+                             lineHeight: 1.16)
+    $0.font = .boldSystemFont(ofSize: 18)
+    $0.numberOfLines = 0
+    $0.alpha = 0
+  }
+  
+  private lazy var headerSecondLabel = UILabel().then {
+    $0.setTextWithLineHeight(text: "미리보기로 원하는\n여행인지 확인하세요",
+                             lineHeight: 1.16)
+    $0.font = .boldSystemFont(ofSize: 18)
+    $0.numberOfLines = 0
+    $0.alpha = 0
+  }
+  
+  private lazy var headerThirdLabel = UILabel().then {
+    $0.setTextWithLineHeight(text: "여행 일정을 구매하여\n알찬 여행 정보를 만나보세요",
+                             lineHeight: 1.16)
+    $0.font = .boldSystemFont(ofSize: 18)
+    $0.numberOfLines = 0
+    $0.alpha = 0
+  }
+  
+
   // MARK: - Life Cycle Part
   
     override func viewDidLoad() {
