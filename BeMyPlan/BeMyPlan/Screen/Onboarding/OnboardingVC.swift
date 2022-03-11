7216e6855f108225ff86e51e84115c5c0e35e225
i-colours-u <i.colours.u@gmail.com>
2022-03-11 12:25:50 +0900

    [feat] 온보딩뷰 vc 생성

---
+++ BeMyPlan/BeMyPlan/Screen/Onboarding/OnboardingVC.swift
@@ -0,0 +1,34 @@
+//
+//  OnboardingVC.swift
+//  BeMyPlan
+//
+//  Created by 송지훈 on 2022/03/11.
+//
+
+import UIKit
+
+class OnboardingVC: UIViewController {
+
+  // MARK: - Vars & Lets Part
+
+
+  // MARK: - UI Component Part
+
+  
+  // MARK: - Life Cycle Part
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+         
+    }
+    
+  // MARK: - IBAction Part
+
+  
+  // MARK: - Custom Method Part
+
+  
+  // MARK: - @objc Function Part
+
+}
+// MARK: - Extension Part
