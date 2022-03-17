ca9fce3a2a4e558b74914fd3ea7fadc743631d1a
i-colours-u <i.colours.u@gmail.com>
2022-03-17 22:07:11 +0900

    [del] 중복 파일 삭제

--- BeMyPlan/BeMyPlan/Screen/Plan/PlanPreview/UseCase/Model/PreviewBodyData.swift
+++ BeMyPlan/BeMyPlan/Screen/Plan/PlanPreview/UseCase/Model/PreviewBodyData.swift
@@ -2,14 +2,7 @@
 //  PreviewBodyData.swift
 //  BeMyPlan
 //
-//  Created by 송지훈 on 2022/03/14.
+//  Created by 송지훈 on 2022/03/17.
 //
 
 import Foundation
-
-extension PlanPreview {
-  struct BodyData {
-    var photos: [PlanPreview.PhotoData]
-    var summary: PlanPreview.SummaryData?
-  }
-}
