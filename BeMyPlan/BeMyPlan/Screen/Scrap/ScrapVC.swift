01c583bbfcb4fe1b9f31507b2f6710a7f74198ad
i-colours-u <i.colours.u@gmail.com>
2022-06-01 17:09:20 +0900

    [feat] 스크랩 취소하고나서 탭을 움직일때 데이터가 갱신될 수 있도록 처리

--- BeMyPlan/BeMyPlan/Screen/Scrap/ScrapVC.swift
+++ BeMyPlan/BeMyPlan/Screen/Scrap/ScrapVC.swift
@@ -15,28 +15,38 @@ class ScrapVC: UIViewController {
   @IBOutlet var scrapEmptyView: ScrapEmptyContainerView!
   
   // MARK: - Vars & Lets Part
-  var scrapDataList:[ScrapDataGettable] = []
-  var sortCase : FilterSortCase = .recently
+  var scrapDataList: [ScrapDataGettable] = []
+  var sortCase: FilterSortCase = .recently
+  var nextCursor: Int = 0
   
   // MARK: - Life Cycle Part
   override func viewDidLoad() {
     super.viewDidLoad()
     setUI()
+    registerObserverActions()
     bottomSheetNotification()
-    fetchScrapListData(sort: .recently)
     fetchRecomendData()
   }
   
+  override func viewWillAppear(_ animated: Bool) {
+    fetchScrapListData(sort: .recently)
+  }
+
   private func setUI(){
     scrapEmptyView.isHidden = true
   }
     
   private func fetchScrapListData(lastId: Int? = nil,sort: FilterSortCase) {
-    BaseService.default.getScrapList(lastId: lastId, sort: sort) { result in
+    guard nextCursor != -1 else { return }
+    var sortCase: FilterSortCase
+    if sort == .scrapCount { sortCase = .orderCount }
+    else { sortCase = sort }
+    BaseService.default.getScrapList(lastId: lastId, sort: sortCase) { result in
       result.success { entity in
         guard let entity = entity else {return}
         self.scrapEmptyView.isHidden = !entity.contents.isEmpty
         self.scrapView.isHidden = entity.contents.isEmpty
+        self.nextCursor = entity.nextCursor
 
         self.scrapView.scrapDataList = entity.contents
       }.catch { _ in
@@ -60,8 +70,28 @@ class ScrapVC: UIViewController {
     addObserverAction(.filterBottomSheet) { _ in
       let vc = UIStoryboard(name: "TravelSpot", bundle: nil).instantiateViewController(withIdentifier: "TravelSpotFilterVC") as! TravelSpotFilterVC
       vc.filterStatus = self.sortCase
-      vc.filterClicked = { filter in self.sortCase = filter }
+      vc.filterClicked = { filter in
+        self.sortCase = filter
+        self.fetchScrapListData(sort: filter)
+      }
       self.presentPanModal(vc)
     }
   }
+  
+  private func registerObserverActions() {
+    addObserverAction(.moveHomeTab) { [weak self] _ in
+      self?.fetchScrapListData(sort: .recently)
+    }
+    
+    addObserverAction(.moveTravelSpotTab) { [weak self] _ in
+      self?.fetchScrapListData(sort: .recently)
+    }
+    
+    addObserverAction(.moveMyPlanTab) { [weak self] _ in
+      self?.fetchScrapListData(sort: .recently)
+    }
+  }
+
+  
+  
 }
