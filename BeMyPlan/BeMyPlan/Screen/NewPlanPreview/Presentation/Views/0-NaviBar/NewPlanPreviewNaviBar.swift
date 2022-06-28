a2653836cd700f7f6795841c85aab3f05bcb2234
i-colours-u <i.colours.u@gmail.com>
2022-06-28 16:24:21 +0900

    [feat] NewPlanPreview Navi bar 정의 완료

--- BeMyPlan/BeMyPlan/Screen/NewPlanPreview/Presentation/Views/0-NaviBar/NewPlanPreviewNaviBar.swift
+++ BeMyPlan/BeMyPlan/Screen/NewPlanPreview/Presentation/Views/0-NaviBar/NewPlanPreviewNaviBar.swift
@@ -8,11 +8,27 @@
 import UIKit
 
 class NewPlanPreviewNaviBar: XibView{
+  
+  @IBOutlet private var backButton: UIButton!
+  @IBOutlet var shareButton: UIButton!
+  
+  @IBOutlet var backgroundView: UIView!
+  @IBOutlet var backButtonCircleView: UIView!
+  @IBOutlet var shareButtonCircleView: UIView!
+  
   override init(frame: CGRect) {
     super.init(frame: frame)
   }
   
   required init?(coder aDecoder: NSCoder) {
     super.init(coder: aDecoder)
+  }
+  
+  private func configureUI() {
+    backButtonCircleView.layer.cornerRadius = 100
+    shareButtonCircleView.layer.cornerRadius = 100
+    backgroundView.alpha = 0
+    backButtonCircleView.alpha = 1
+    shareButtonCircleView.alpha = 1
   }
 }
