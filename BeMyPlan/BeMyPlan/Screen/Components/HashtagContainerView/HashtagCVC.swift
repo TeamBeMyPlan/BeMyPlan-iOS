f571f0545bc37e60a9e2f6e2c66fe373c2c0b554
i-colours-u <i.colours.u@gmail.com>
2022-06-29 01:14:15 +0900

    [feat] HashtagCVC 생성

---
+++ BeMyPlan/BeMyPlan/Screen/Components/HashtagContainerView/HashtagCVC.swift
@@ -0,0 +1,33 @@
+//
+//  HashtagCVC.swift
+//  BeMyPlan
+//
+//  Created by 송지훈 on 2022/06/29.
+//
+
+import UIKit
+
+class HashtagCVC: UICollectionViewCell,UICollectionViewRegisterable {
+  
+  var viewModel: HashtagCellViewModel
+  static var isFromNib: Bool = true
+  
+  @IBOutlet var backgroundContentView: UIView!
+  @IBOutlet var titleLabel: UILabel!
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    configureUI()
+  }
+}
+
+extension HashtagCVC {
+  private func configureUI() {
+    backgroundContentView.layer.cornerRadius = 2
+    titleLabel.text = viewModel.title
+  }
+}
+
+struct HashtagCellViewModel {
+  let title: String
+}
