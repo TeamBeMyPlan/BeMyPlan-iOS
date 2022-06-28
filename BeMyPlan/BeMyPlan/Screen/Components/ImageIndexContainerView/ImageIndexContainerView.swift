0eb8a0c5e72c6f3a9aa1a997f70ac731864cb8a3
i-colours-u <i.colours.u@gmail.com>
2022-06-29 02:43:55 +0900

    [feat] ImageIndexContainerView 완성

--- BeMyPlan/BeMyPlan/Screen/Components/ImageIndexContainerView/ImageIndexContainerView.swift
+++ BeMyPlan/BeMyPlan/Screen/Components/ImageIndexContainerView/ImageIndexContainerView.swift
@@ -8,11 +8,37 @@
 import UIKit
 
 class ImageIndexContainerView: XibView{
+  var viewModel: ImageIndexContainerViewModel! {
+    didSet { setLabelIndex() }
+  }
+  @IBOutlet var backgroundContentView: UIView!
+  @IBOutlet var currentIndexLabel: UILabel!
+  @IBOutlet var totalIndexLabel: UILabel!
+  
   override init(frame: CGRect) {
     super.init(frame: frame)
+    configureUI()
   }
   
   required init?(coder aDecoder: NSCoder) {
     super.init(coder: aDecoder)
+    configureUI()
   }
+}
+
+extension ImageIndexContainerView {
+  private func configureUI() {
+    backgroundContentView.layer.cornerRadius = 100
+    self.isHidden = viewModel.totalIndex == 1
+  }
+  
+  private func setLabelIndex() {
+    currentIndexLabel.text = String(viewModel.currentIndex + 1)
+    totalIndexLabel.text = String(viewModel.totalIndex + 1)
+  }
+}
+
+struct ImageIndexContainerViewModel {
+  var currentIndex: Int
+  let totalIndex: Int
 }
