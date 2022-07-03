29b2026d5f5262a7fab0a7d3e4357b9ccff3e9a0
i-colours-u <i.colours.u@gmail.com>
2022-07-04 03:01:58 +0900

    [feat] NewPlanMainContentsTVC 추가

---
+++ BeMyPlan/BeMyPlan/Screen/NewPlanPreview/Presentation/Views/5-MainContents/Cells/NewPlanMainContentsTVC.swift
@@ -0,0 +1,28 @@
+//
+//  NewPlanMainContentsTVC.swift
+//  BeMyPlan
+//
+//  Created by 송지훈 on 2022/07/04.
+//
+
+import UIKit
+
+class NewPlanMainContentsTVC: UITableViewCell,UITableViewRegisterable {
+  static var isFromNib: Bool = true
+  var viewModel: NewPlanMainContentsCellViewModel!
+  
+  @IBOutlet var imageSlideShowView: ImageSlideShow!
+  @IBOutlet var contentTextView: UITextView!
+}
+
+extension NewPlanMainContentsTVC {
+  private func configureUI() {
+    contentTextView.removeMargin()
+    imageSlideShowView.viewModel = .init(imgList: self.viewModel.imgURLs)
+  }
+}
+
+struct NewPlanMainContentsCellViewModel {
+  let imgURLs: [String]
+  let contents: String
+}
